cc6bfc5f461be204352b63a1e1baffb6fa310048
unknown <m.rutkowski0909@gmail.com>
2022-12-05 13:37:17 +0100

    ui: navbar

--- libs/web/shell/src/lib/web-shell.module.ts
+++ libs/web/shell/src/lib/web-shell.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebShellContainerComponent } from './containers/web-shell-container/web-shell-container.component';
 import { WebShellRoutingModule } from './web-shell-routing.module';
+import { WebSharedNavbarFeatureComponent } from '@onboarding/web/shared/navbar/feature';
 
 @NgModule({
-  imports: [CommonModule, WebShellRoutingModule],
+  imports: [
+    CommonModule,
+    WebShellRoutingModule,
+    WebSharedNavbarFeatureComponent,
+  ],
   declarations: [WebShellContainerComponent],
 })
 export class WebShellModule {}

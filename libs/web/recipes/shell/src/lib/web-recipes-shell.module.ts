0295614c2280a8351e942a4b8101bb3e7ad34ea7
unknown <m.rutkowski0909@gmail.com>
2022-12-05 14:05:49 +0100

    ui: recipies shell layout

--- libs/web/recipes/shell/src/lib/web-recipes-shell.module.ts
+++ libs/web/recipes/shell/src/lib/web-recipes-shell.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { WebRecipiesShellRoutingModule } from './web-recipes-shell-routing.module';
 import { RecipesShellContainerComponent } from './containers/recipes-shell-container.component';
 
 @NgModule({
-  imports: [CommonModule, WebRecipiesShellRoutingModule],
+  imports: [CommonModule, WebRecipiesShellRoutingModule, MatCardModule],
   declarations: [RecipesShellContainerComponent],
 })
 export class WebRecipesShellModule {}

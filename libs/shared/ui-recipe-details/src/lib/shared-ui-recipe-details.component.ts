9fac25d10c3af7071b8f8dfd33209c179d7479f3
unknown <m.rutkowski0909@gmail.com>
2022-12-06 14:38:29 +0100

    util: preparation time pipe

--- libs/shared/ui-recipe-details/src/lib/shared-ui-recipe-details.component.ts
+++ libs/shared/ui-recipe-details/src/lib/shared-ui-recipe-details.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiRecipeDetails } from './ui-recipe-details.interface';
+import { PreparationTimePipe } from '@onboarding/shared/util-preparation-time-pipe';
 
 @Component({
   selector: 'onboarding-ui-recipe-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PreparationTimePipe],
   templateUrl: './shared-ui-recipe-details.component.html',
   styleUrls: ['./shared-ui-recipe-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,

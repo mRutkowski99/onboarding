5bb9fe8b2da8d80f2f0b28e6835afc271c7f04e8
unknown <m.rutkowski0909@gmail.com>
2022-12-11 18:21:15 +0100

    data service

--- libs/web/recipes/edit-recipe/data-access/src/index.ts
+++ libs/web/recipes/edit-recipe/data-access/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/web-recipes-edit-recipe-data-access.module';
+export * from './lib/services/edit-recipe-data.service';
--- libs/web/recipes/edit-recipe/data-access/src/lib/web-recipes-edit-recipe-data-access.module.ts
+++ libs/web/recipes/edit-recipe/data-access/src/lib/web-recipes-edit-recipe-data-access.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedDataAccessModule } from '@onboarding/shared/data-access';
+import { EditRecipeDataService } from './services/edit-recipe-data.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedDataAccessModule],
+  providers: [EditRecipeDataService],
 })
 export class WebRecipesEditRecipeDataAccessModule {}
